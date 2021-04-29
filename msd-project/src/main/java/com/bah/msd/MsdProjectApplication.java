d32fa98beeb86e0b146bb97e69931309942a6228
adalsantana <asantan6@protonmail.com>
2021-04-29 19:07:09 -0400

    inital commit

---
+++ msd-project/src/main/java/com/bah/msd/MsdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello test Code begins");
+		SpringApplication.run(MsdProjectApplication.class, args);
+	}
+}
