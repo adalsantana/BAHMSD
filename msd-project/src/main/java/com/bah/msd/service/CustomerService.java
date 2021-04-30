f256963fde07d0e1ce3ca7e0763cd7c4f828e6a3
adalsantana <asantan6@protonmail.com>
2021-04-30 10:56:32 -0400

    initial customer setup. Day 2 customer tasks.

---
+++ msd-project/src/main/java/com/bah/msd/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.bah.msd.service;
+
+import java.util.ArrayList;
+
+import com.bah.msd.model.Customer;
+
+public interface CustomerService {
+	public ArrayList<Customer> getAllCustomers();
+}
---
+++ msd-project/src/main/java/com/bah/msd/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bah.msd.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.bah.msd.model.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+	ArrayList<Customer> customerList = new ArrayList<Customer>();
+	
+	public CustomerServiceImpl() {
+		Customer customer1 = new Customer(1,"Lynton","pass", "jl@email.com");
+		Customer customer2 = new Customer(2,"Adal","guess", "asc@email.com");
+		Customer customer3 = new Customer(1,"Brandon","foo", "bw@email.com");
+		customerList.add(customer1);
+		customerList.add(customer2);
+		customerList.add(customer3);
+	}
+	public ArrayList<Customer> getAllCustomers(){
+		return customerList;
+	}
+
+}
