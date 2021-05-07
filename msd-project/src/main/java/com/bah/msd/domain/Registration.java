a6a6135dab879715cba5782605e1b9737ad40e6c
adalsantana <asantan6@protonmail.com>
2021-05-06 20:15:11 -0400

    generated getters and setters for registration

--- msd-project/src/main/java/com/bah/msd/domain/Registration.java
+++ msd-project/src/main/java/com/bah/msd/domain/Registration.java
@@ -24,7 +24,37 @@ public class Registration {
 		this.registration_date = registration_date;
 		this.notes = notes;
 	}
+
+	public long getEvent_id() {
+		return event_id;
+	}
+
+	public void setEvent_id(long event_id) {
+		this.event_id = event_id;
+	}
+
+	public long getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(long customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public Date getRegistration_date() {
+		return registration_date;
+	}
+
+	public void setRegistration_date(Date registration_date) {
+		this.registration_date = registration_date;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
 	
-	
-	//return null;
 }
