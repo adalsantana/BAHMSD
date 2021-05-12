843f2ee5f7276e591fabc66e04318f7016001e6d
adalsantana <asantan6@protonmail.com>
2021-05-12 13:54:14 -0400

    tied ID to customer sql primary key id

--- msd-project/src/main/java/com/bah/msd/domain/Customer.java
+++ msd-project/src/main/java/com/bah/msd/domain/Customer.java
@@ -1,8 +1,13 @@
 package com.bah.msd.domain;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	int id;
 	@Column(name = "CUSTOMER_NAME")
 	String name;
