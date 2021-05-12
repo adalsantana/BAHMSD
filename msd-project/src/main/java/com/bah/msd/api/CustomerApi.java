bfc7110fbac4b1b2992d7042943796d3c9266dd4
adalsantana <asantan6@protonmail.com>
2021-05-12 14:04:18 -0400

    added putCustomer

--- msd-project/src/main/java/com/bah/msd/api/CustomerApi.java
+++ msd-project/src/main/java/com/bah/msd/api/CustomerApi.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,15 @@ public class CustomerApi {
 			}
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> putCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri){
+		if(invalidCustomerCheck(newCustomer)) return ResponseEntity.badRequest().build();
+		newCustomer = repo.save(newCustomer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
+		return response;
 	}
 	
 	@PostMapping
