12a1e8eb846ea5dabe8b7cba40e424ff00087a61
adalsantana <asantan6@protonmail.com>
2021-05-13 19:15:42 -0400

    registration repository

--- msd-project/src/main/java/com/bah/msd/repository/RegistrationRepository.java
+++ msd-project/src/main/java/com/bah/msd/repository/RegistrationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.bah.msd.domain.Registration;
 
 public interface RegistrationRepository extends CrudRepository<Registration, Long>{
-
+	
 }
